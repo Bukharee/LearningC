83557037537ddc3950eb4b9ce0d3563d13590f99
Bukhari Auwal <idrisbukhariauwal@gmail.com>
2026-04-07 06:40:53 +0100

    update

--- Excercises/cel.c
+++ Excercises/cel.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
 int main(){
+
+
 float farh, celsius;
-float higher, lower, step;
-step = 20;
-higher = 300;
-lower = 0;
-celsius = lower;
-while (celsius <= higher) {
+/* while (celsius <= higher) { */
+   for (celsius = UPPER; celsius >= LOWER; celsius = celsius - STEP){
     farh =(((9.0/5.0) * celsius) +32.0);
     printf("%3.0f%6.1f\n", celsius, farh);
-    celsius = celsius + step;
+   /*  celsius = celsius + step; */
 }
 }
